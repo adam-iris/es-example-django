6108d2e92213e226cd726f682e506883f5489dbf
Adam Clark <adam@iris.washington.edu>
2021-07-16 18:03:35 -0700

    Retry consumer if the topic is unknown

--- kafka_example/kafka/consumer.py
+++ kafka_example/kafka/consumer.py
@@ -1,14 +1,33 @@
 import kafka_interface as kafka
 import logging
 from kafka_example.models import ExampleValue
+from es_lib.utils import retry, RetryableError
 
 LOGGER = logging.getLogger(__name__)
+
+
+class RetryableConsumerError(RetryableError):
+    """
+    Indicates a condition where the consumer should retry
+    """
+
+
+def rethrow_error(err):
+    """
+    Convert a low-level kafka error, mainly this is to throw
+    a retryable error if we can
+    """
+    # Topic is unknown
+    name = getattr(err, 'name', '')
+    if name.startswith('UNKNOWN_TOPIC'):
+        raise RetryableConsumerError("Unknown topic")
 
 
 class ExampleConsumer(object):
     """
     Just consumes from the topic
     """
+    topic = 'example'
 
     def consume(self, message):
         """
@@ -16,14 +35,16 @@ class ExampleConsumer(object):
         """
         LOGGER.info("Message: %s", message)
         value = message.get('value', {})
-        ExampleValue.objects.create(**value)
+        obj = ExampleValue(**value)
+        obj.clean()
+        obj.save()
 
+    @retry(count=100, delay=2, backoff=2)
     def run(self):
         """
         Consume messages
         """
-        topic = 'example'
-        consumer = kafka.kafka_consumer(topic)
+        consumer = kafka.kafka_consumer(self.topic)
         LOGGER.info("Starting consumer")
 
         try:
@@ -32,6 +53,9 @@ class ExampleConsumer(object):
                 self.consume(message)
         except Exception as e:
             LOGGER.error("Failed with %s", e)
+            # Maybe rethrow as an alternate type
+            rethrow_error(e)
+            # Otherwise, throw the original
             raise
 
         
